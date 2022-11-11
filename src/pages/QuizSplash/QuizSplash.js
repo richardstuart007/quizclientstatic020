82d1cb0dd72c97100a8cffb6a24491ae67dc569f
Richard <richardstuart007@hotmail.com>
2022-11-12 09:53:15 +1300

    splash links

--- src/pages/QuizSplash/QuizSplash.js
+++ src/pages/QuizSplash/QuizSplash.js
@@ -37,23 +37,31 @@ const QuizSplash = () => {
     <Grid container>
       <Container>
         <Typography variant='h6' sx={{ marginTop: '8px', color: 'blue' }}>
-          STATIC VERSION 010
-        </Typography>
-        <Typography variant='h6' sx={{ marginTop: '8px' }}>
-          This product is in Trial/Development
+          STATIC VERSION 020
         </Typography>
         <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
-          It has been developed by Richard Stuart and is FREE to use/distribute.
+          Developed by Richard Stuart and is FREE to use
         </Typography>
         <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
           Comments you can email me at richardstuart007@hotmail.com
         </Typography>
         <Typography variant='subtitle2' sx={{ marginTop: '32px', color: 'red' }}>
-          PLEASE SWITCH TO THE DATABASE VERSION SO THAT YOU CAN SIGN IN AND HAVE A HISTORY OF YOUR
-          QUIZZES
+          THE DATABASE VERSION KEEPS A HISTORY OF YOUR QUIZZES
         </Typography>
-        <Typography variant='h3' sx={{ marginTop: '8px' }}>
-          <a href='https://richardstuart007.github.io/quizclient010/'>Database Version</a>
+        <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
+          Please switch a database version below
+        </Typography>
+        <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
+          <a href='https://richardstuart007.github.io/quizclient010/'>Database Version 10 GITHUB</a>
+        </Typography>
+        <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
+          <a href='https://quizclient010-production.up.railway.app/'>Database Version 10 RAILWAY</a>
+        </Typography>
+        <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
+          <a href='https://richardstuart007.github.io/quizclient020/'>Database Version 20 GITHUB</a>
+        </Typography>
+        <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
+          <a href='https://quizclient020.netlify.app/'>Database Version 20 NETLIFY</a>
         </Typography>
         <Typography variant='subtitle2' sx={{ marginTop: '32px' }}>
           Alternatively click on the Continue button below to use the static version
